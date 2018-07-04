4b14756e409ae4502df13a002b5556629aaf150d
Brandon Miller <xzealot54x@yahoo.com>
2018-07-04 00:33:44 -0500

    Initial Commit

---
+++ Fibonacci/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fibonacci
+//
+//  Created by Brandon Miller on 7/4/18.
+//  Copyright © 2018 SnotRocket. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
