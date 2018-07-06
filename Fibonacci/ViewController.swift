47d68e3c88da40294aa41881eb09c33aecf7f221
Brandon Miller <xzealot54x@yahoo.com>
2018-07-05 22:14:52 -0500

    Create FIbonacci app and GUI.

--- Fibonacci/ViewController.swift
+++ Fibonacci/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputTextField: UITextField!
+    @IBOutlet weak var outputTextView: UITextView!
+    @IBOutlet weak var calcButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func pressCalcButton(_ sender: Any) {
+        let n : Int = Int(inputTextField.text ?? "0") ?? 0
+        if n == 0 {
+            outputTextView.text = "Invalid"
+            return
+        }
+        var output : String = ""
+        for i in 1...n {
+            print(recurseFibonacci(i))
+            let newVal = recurseFibonacci(i)
+            output += String(newVal) + ", "
+        }
+        outputTextView.text = output
+    }
+    func recurseFibonacci(_ n: Int) -> Int {
+        if (n == 1) || (n == 2) {
+            return 1
+        }
+        return recurseFibonacci(n - 1) + recurseFibonacci(n - 2)
+    }
 }
 
